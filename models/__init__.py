0ad413790f93aa7990fccc1c34fb4d2734a0ced6
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-06-22 00:47:39 +0100

    Added initialization for database storage mode

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+"""
+This module instantiates an object depending on the value of the environment
+variable 'HBNB_TYPE_STORAGE'.
+
+if "HBNB_TYPE_STORAGE" == 'db':
+    instantiate the class DBStorage
+else:
+    FileStorage
+
+This 'switch' allows us to change storage type directly by using an
+environment variable
+"""
+import os
 
 
-storage = FileStorage()
-storage.reload()
+if os.getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
