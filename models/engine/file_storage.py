44d4ab3e931fc292bf1cd4ee9a239e7f78dc8b16
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-06-22 00:43:58 +0100

    Modified class to handle database storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,13 +8,25 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, obj=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        ret = {}
+        if obj is not None:
+            obj_name = obj.__name__
+            for model in FileStorage.__objects:
+                model_name = model.split(".")[0]
+                if model_name == obj_name:
+                    ret[model] = FileStorage.__objects[model]
+            for obj in ret:
+                if "__class__" in ret[obj]:
+                    del (ret[obj]["__class__"])
+            return (ret)
+        return (FileStorage.__objects)
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[key] = obj.to_dict()
 
     def save(self):
         """Saves storage dictionary to file"""
@@ -22,7 +34,7 @@ class FileStorage:
             temp = {}
             temp.update(FileStorage.__objects)
             for key, val in temp.items():
-                temp[key] = val.to_dict()
+                temp[key] = val
             json.dump(temp, f)
 
     def reload(self):
@@ -45,6 +57,14 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    class_name = key.split(".")[0]
+                    self.all()[key] = classes[class_name](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Deletes obj if it exists in the __objects attribute"""
+        if obj is not None:
+            key = "{:s}.{:s}".format(type(obj).__name__, obj.id)
+            if key in FileStorage.__objects:
+                del (FileStorage.__objects[key])
