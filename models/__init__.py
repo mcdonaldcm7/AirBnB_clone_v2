3cf1c0f0283099886fd8522d6e9651690873fbca
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-06-22 17:11:30 +0100

    Reverted code

--- models/__init__.py
+++ models/__init__.py
@@ -1,24 +1,7 @@
 #!/usr/bin/python3
-"""
-This module instantiates an object depending on the value of the environment
-variable 'HBNB_TYPE_STORAGE'.
-
-if "HBNB_TYPE_STORAGE" == 'db':
-    instantiate the class DBStorage
-else:
-    FileStorage
-
-This 'switch' allows us to change storage type directly by using an
-environment variable
-"""
-import os
+"""This module instantiates an object of class FileStorage"""
+from models.engine.file_storage import FileStorage
 
 
-if os.getenv("HBNB_TYPE_STORAGE") == "db":
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
-    storage.reload()
-else:
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
-    storage.reload()
+storage = FileStorage()
+storage.reload()
