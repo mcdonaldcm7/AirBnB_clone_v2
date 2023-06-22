5124dbe95a8b9b9fa9f452f7dd1232eff4f56612
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-06-22 17:13:01 +0100

    Reverted code

--- models/state.py
+++ models/state.py
@@ -1,29 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
+class State(BaseModel):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete")
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    @property
-    def get_cities(self):
-        """
-        Returns the list of City instances with state_id equals to current
-        State.id
-        """
-        from models import storage
-        state_instances = storage.all(State)
-        state_city = [
-                state for state in state_instances.values()
-                if state.state_id == self.id
-                ]
-        return (state_city)
+    name = ""
