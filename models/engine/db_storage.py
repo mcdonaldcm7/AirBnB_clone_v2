5f41ff5fbcf7ca1a66e917522bec8df42c42f033
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-06-24 15:06:46 +0100

    Definition for DBStorage class

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-
-
+"""This module contains the definition for the DBStorage class which handles
+the ORM operations we want to perform and gives us an abstracted method of
+swapping between File Storage and Database Storage
+"""
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 class DBStorage:
+    """
+    This class integrates the ORM concept into this project by using the
+    SQLAlchemy library to make queries to the database and perform other
+    file storage operations
+    """
     __engine = None
     __session = None
 
@@ -18,12 +25,14 @@ class DBStorage:
         is_test = True if os.getenv("HBNB_ENV") == "test" else False
         self.__engine = create_engine(
                 "mysql+mysqldb://{}:{}@localhost/{}".format(
-                    user, psswd, db), pool_pre_ping=True)
+                    user, psswd, db),
+                pool_pre_ping=True
+                )
 
     def all(self, cls=None):
         """
-        Make a query on the current database session (self.__session) all
-        objects depending on the class name (argument cls)
+        Make a query on the current database session all objects depending of
+        the class name (argument cls)
         """
         from models.state import State
         from models.city import City
@@ -38,11 +47,8 @@ class DBStorage:
                 }
         dictionary = {}
         if cls is None:
-            objects = (self.__session
-                       .query(State)
-                       .join(City)
-                       .filter(State.id == City.state_id)
-                       .all())
+            objects = self.__session.query(
+                    State, City, User, Place, Amenity, Review).all()
             for obj in objects:
                 key = "{}.{}".format(obj.name, type(obj).__name__)
                 dictionary[key] = obj
@@ -58,7 +64,7 @@ class DBStorage:
         self.__session.add(obj)
 
     def save(self):
-        """Commits all the changes of the current databse session"""
+        """Commits all the changes of the current database session"""
         self.__session.commit()
 
     def delete(self, obj=None):
