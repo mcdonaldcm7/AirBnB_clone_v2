a65dc7aa84a796d66265fe55b5de030d652b8476
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-07-21 12:08:27 +0100

    Used strict_slashes option

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -36,7 +36,7 @@ def python_path(text):
     return ("Python {}".format(text.replace('_', ' ')))
 
 
-@app.route("/number/<int:n>")
+@app.route("/number/<int:n>", strict_slashes=False)
 def number_path(n):
     """
     Handles the request and the arguments, also ensuring the argument
