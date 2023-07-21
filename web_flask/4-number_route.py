05c523e7186dc733686ff1ed248caf48e5114ed1
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-07-21 12:03:50 +0100

    Handles request to /number/n, n must be a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""This module starts a simple Flask web application"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Handles request directed to root path"""
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Handles request directed to the path /hbnb"""
+    return ("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_path(text):
+    """Handles request directed to the path /c/<text>"""
+    return ("C {}".format(text.replace('_', ' ')))
+
+
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_path(text):
+    """
+    Handles the request and the argument passed to the path
+    /python/(<text>)
+    """
+    return ("Python {}".format(text.replace('_', ' ')))
+
+
+@app.route("/number/<int:n>")
+def number_path(n):
+    """
+    Handles the request and the arguments, also ensuring the argument
+    passed is a number
+    """
+    return ("{} is a number".format(n))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
