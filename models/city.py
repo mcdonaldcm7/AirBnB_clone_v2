a9ae4f0946a8ca7d9613c53c5313ed0cc5dd7b27
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-07-21 15:56:42 +0100

    Used a control flow to decide definition of class

--- models/city.py
+++ models/city.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+import os
 
 
-class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    __tablename__ = "cities"
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=True)
-    name = Column(String(128), nullable=False)
-    state = relationship("State", back_populates="cities")
+if os.getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.base_model import BaseModel, Base
+    from models.state import State
+    from sqlalchemy import Column, String, ForeignKey
+    from sqlalchemy.orm import relationship
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    class City(BaseModel, Base):
+        """ The city class, contains state ID and name """
+        __tablename__ = "cities"
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=True)
+        name = Column(String(128), nullable=False)
+        state = relationship("State", back_populates="cities")
+else:
+    from models.base_model import BaseModel
+
+    class City(BaseModel):
+        """ The city class, contains state ID and name """
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
