8a23af18317021f7637fe244d02f60ee0a51fbf9
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-07-21 16:01:39 +0100

    Used a control flow to decide definition of class

--- models/user.py
+++ models/user.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+import os
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
-    places = relationship(
-            "Place", back_populates="user", cascade="all, delete"
-            )
-    reviews = relationship(
-            "Review", back_populates="user", cascade="all, delete"
-            )
+if os.getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.base_model import BaseModel, Base
+    from sqlalchemy import Column, String
+    from sqlalchemy.orm import relationship
+
+    class User(BaseModel):
+        """This class defines a user by various attributes"""
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+        places = relationship(
+                "Place", back_populates="user", cascade="all, delete"
+                )
+        reviews = relationship(
+                "Review", back_populates="user", cascade="all, delete"
+                )
+else:
+    from models.base_model import BaseModel
+
+    class User(BaseModel):
+        """This class defines a user by various attributes"""
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
