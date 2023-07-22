bb954763841c2a24a612a4349e6fee3efb4f9a53
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-07-22 11:27:29 +0100

    Added to_dict for db definition

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import os
+import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
@@ -13,14 +14,26 @@ if os.getenv("HBNB_TYPE_STORAGE") == "db":
     class BaseModel:
         """A base class for all hbnb models"""
 
-        id = Column(String(60), unique=True, nullable=False, primary_key=True)
+        id = Column(String(60), unique=True, nullable=False,
+                    primary_key=True, default=str(uuid.uuid4()))
         created_at = Column(DateTime,
                             default=datetime.utcnow(), nullable=False)
         updated_at = Column(DateTime,
                             default=datetime.utcnow(), nullable=False)
+        
+        def to_dict(self):
+            """Convert instance into dict format"""
+            dictionary = {}
+            dictionary.update(self.__dict__)
+            dictionary.update({'__class__':
+                              (str(type(self)).split('.')[-1]).split('\'')[0]})
+            dictionary['created_at'] = self.created_at
+            dictionary['updated_at'] = self.updated_at
+            key = "_sa_instance_state"
+            if key in dictionary:
+                del (dictionary[key])
+            return dictionary
 else:
-    import uuid
-
     class BaseModel:
         def __init__(self, *args, **kwargs):
             """Instatntiates a new model"""
